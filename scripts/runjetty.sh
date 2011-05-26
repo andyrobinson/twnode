1a697310961ef53fea9fd494b2e8e97b3965f20f
Andy Robinson <andy.robinson@thoughtworks.com>
2011-05-26 23:11:02 +0100

    Adding Jetty server files

--- scripts/runjetty.sh
+++ scripts/runjetty.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-javac -sourcepath "/Users/arobinson/Documents/workspace/SimpleJetty/src" -d "~/Jetty/webapps/basic/WEB-INF/classes" -classpath "~/Jetty/lib/servlet-api-2.5.jar"
+cp ~/github/twnode/java/jetty/bin/*.class ~/Jetty/webapps/jetty/WEB-INF/classes
+cp ~/github/twnode/java/jetty/web.xml ~/Jetty/webapps/jetty/WEB-INF
+cd ~/Jetty
+java -jar start.jar
